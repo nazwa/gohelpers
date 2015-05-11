ea6ce85c17f0f317cb784b066c0451db9e76de98
Maciej Ka <zorleq@hotmail.com>
2015-05-11 16:44:35 +0100

    new tokens

---
+++ tokens/tokens.go
@@ -0,0 +1,40 @@
+package tokens
+
+import (
+	"crypto/rand"
+	"fmt"
+)
+
+var Token25 chan string
+var Token50 chan string
+
+func generator() {
+	fmt.Println("Running")
+	for {
+
+		data := make([]byte, 50)
+
+		if _, err := rand.Read(data); err != nil {
+			fmt.Println(err)
+			continue
+		}
+		select {
+		case Token25 <- fmt.Sprintf("%x", data):
+		case Token50 <- fmt.Sprintf("%x", data):
+		}
+
+	}
+}
+
+func SpawnGenerators(count int64) {
+	var x int64
+	for x = 0; x < (count); x++ {
+		go generator()
+	}
+}
+
+func init() {
+	Token25 = make(chan string)
+	Token50 = make(chan string)
+	SpawnGenerators(1)
+}
