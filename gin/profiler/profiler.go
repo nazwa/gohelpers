e9bc333d4bad2fdb3ec8df501bef7d110282436d
Maciej Ka <zorleq@hotmail.com>
2015-08-12 16:12:05 +0100

    disabled profiler autostart

--- gin/profiler/profiler.go
+++ gin/profiler/profiler.go
@@ -6,10 +6,8 @@ import (
 )
 
 func AttachRoutes(group *gin.RouterGroup) {
-	group.GET("/profiler/info.html", gin.WrapF(profiler.MemStatsHTMLHandler))
+	group.GET("/profiler", gin.WrapF(profiler.MemStatsHTMLHandler))
 	group.GET("/profiler/info", gin.WrapF(profiler.ProfilingInfoJSONHandler))
 	group.GET("/profiler/start", gin.WrapF(profiler.StartProfilingHandler))
 	group.GET("/profiler/stop", gin.WrapF(profiler.StopProfilingHandler))
-
-	profiler.StartProfiling()
 }
